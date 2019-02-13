f5482a1db4bc7386c59eba63d20193f7618014bb
KieranVieira <kieranvieira@live.com>
2019-02-13 15:49:06 -0500

    Added post request for user-router

--- routes/user-router.js
+++ routes/user-router.js
@@ -3,16 +3,38 @@ const db = require('../data/helpers/userDb')
 
 const router = express.Router();
 
+router.post('/', (req, res) => {
+    const userData = req.body;
+    try {
+        db
+            .insert(userData)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(error =>{
+                res.status(400).json({
+                    message: "Please provide a name for the user",
+                    error
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            message: "Server could not add the user to the database",
+            error
+        })
+    }
+});
+
 router.get('/', (req, res) => {
     db
         .get()
         .then(posts => {
             res.status(200).json(posts)
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({ error: "Server could not fetch users", err })
+        .catch(error => {
+            res.status(500).json({ message: "Server could not fetch users", error })
         })
 });
+
 
 module.exports = router;
