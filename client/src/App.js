6443bfd801cb732e9f115660963b4ca0b410e6d8
KieranVieira <kieranvieira@live.com>
2019-02-13 18:58:59 -0500

    Setup dependencys in index.js and prepared client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>React App</h1>
+        <Route path="/" render={props => <h1 {...props}>React-App-Homepage</h1>}/>
       </div>
     );
   }
