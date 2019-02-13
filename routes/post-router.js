778d360af7a5f5d5173069581975627dcec3ab42
KieranVieira <kieranvieira@live.com>
2019-02-13 16:42:51 -0500

    added get request for post-router

--- routes/post-router.js
+++ routes/post-router.js
@@ -4,7 +4,23 @@ const db = require('../data/helpers/postDb')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('post directory')
+    db
+        .get()
+        .then(posts => {
+            if(posts){
+                res.status(200).json(posts)
+            }else{
+                res.status(404).json({
+                    message: "No posts found"
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Server could not find posts",
+                error
+            })
+        })
 });
 
 module.exports = router;
