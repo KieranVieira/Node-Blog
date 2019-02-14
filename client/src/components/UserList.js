5e661589cf11a9cc448943f37b8de46219d26d7c
KieranVieira <kieranvieira@live.com>
2019-02-14 17:40:25 -0500

    Added mild styling

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const UserCard = styled.div`
+    margin: 25px 10px;
+    a{
+        padding: 15px;
+        border: 1px solid lightgray;
+        color: black;
+        font-size: 1.7rem;
+        text-decoration: none;
+        transition: 0.2s;
+        &:hover{
+            box-shadow: 0px 3px 15px rgba(0,0,0,.25);
+        }
+
+    }
+`;
+
+const UserContainer = styled.div`
+    width: 80%;
+    margin: 0 auto; 
+    display: flex;
+    flex-wrap: wrap;
+`;
 
 const UserList = props => {
     return(
-        props.users.map(user => {
-            return <Link to={`/user/${user.id}`}>{user.name}</Link>
-        })
+        <UserContainer>
+            {props.users.map(user => {
+                return <UserCard>
+                    <Link to={`/user/${user.id}`}>{user.name}</Link>
+                </UserCard>
+            })}
+        </UserContainer>
     )
 }
 
--- client/src/views/SingleUserView.js
+++ client/src/views/SingleUserView.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { fetchUserPosts } from '../store/actions';
+
+const PostHeader = styled.h1`
+    text-align: center;
+`;
+
+const BackButton = styled.div`
+    margin-top: 15px;
+    a{
+        padding: 10px 15px;
+        color: black;
+        text-decoration: none;
+        border: 1px solid lightgray;
+        transition: 0.2s;
+        &:hover{
+            background-color: black;
+            color: white;
+        }        
+    }
+`;
+
+const Post = styled.h2`
+    padding: 15px;
+    border: 1px solid lightgray;
+    color: black;
+    font-size: 1.5rem;
+    text-decoration: none;
+    transition: 0.2s;
+    &:hover{
+        box-shadow: 0px 3px 15px rgba(0,0,0,.25);
+    }
+`
 
 class SingleUserView extends React.Component{
     componentDidMount(){
@@ -12,14 +44,14 @@ class SingleUserView extends React.Component{
     render(){
         return(
             <>
-                <Link to="/">back</Link>
+                <BackButton><Link to="/">Home</Link></BackButton>
                 {this.props.isFetchingUsersPosts ? 
-                <h1>Loading Posts...</h1> : 
+                <PostHeader>Loading Posts...</PostHeader> : 
                 <>
-                    <h1>Posts:</h1>
+                    <PostHeader>Posts:</PostHeader>
                     {this.props.posts.length > 0 ? 
                     this.props.posts.map(post => {
-                        return <h1>{post.text}</h1>
+                        return <Post>{post.text}</Post>
                     }):
                     <h1>This user has no posts</h1>}
                 </>
